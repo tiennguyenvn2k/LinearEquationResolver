b46bc0ae9b88d6b80038ac5fca8402a84aff432e
tiennguyenvn2k <hiiamgamer2000@gmail.com>
2023-12-08 21:19:27 +0700

    hoan thanh bai tap

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        float a,b;
+        Console.WriteLine("Moi nhap he so a va b: ");
+        a=float.Parse(Console.ReadLine());
+        b=float.Parse(Console.ReadLine());
+        if(a!=0)
+        {
+            Console.WriteLine("Phuong trinh co 1 nghiem duy nhat la: " + (-b)/a);
+        }
+        else
+        {
+            if(b==0)
+            {
+                Console.WriteLine("Phuong trinh co vo so nghiem");
+            }
+            else
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+        }
+    }
+}
